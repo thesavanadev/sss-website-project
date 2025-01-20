940fa489a71e7889de2d82531eb04de6f0ed617b
Michael Owuor <michael.owuor@gmail.com>
2025-01-20 23:03:19 +0300

    feat: update environment variables for Resend API and streamline server URL handling

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-				port: "3000",
-			},
-			{
-				protocol: "https",
-				hostname: process.env.NEXT_PUBLIC_SERVER_URL!,
-			},
+			(() => {
+				const url = new URL(process.env.NEXT_PUBLIC_SERVER_URL!);
+
+				return {
+					protocol: url.protocol.replace(":", "") as "http" | "https",
+					hostname: url.hostname,
+					port: url.hostname === "localhost" ? url.port : undefined,
+				};
+			})(),
 		],
 	},
 };
